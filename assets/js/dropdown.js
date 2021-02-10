504a84ac3c739f2fe56e7dbc6f977ea323431362
Andrieli <andrieliluci@gmail.com>
2021-02-10 13:05:49 -0300

    dropdown

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,19 +1,15 @@
-var drop = document.querySelector('.dropdown')
 var showList = document.querySelector('.show-list');
 var list = document.querySelector('.dropdown__list');
 var arrow = document.querySelector('.open-arrow');
 
 function show() {
     list.classList.toggle("active");
-    arrow.classList.add('close-arrow');
-    arrow.classList.remove('open-arrow');
 }
   
 
 window.onclick = function(event) {
     if (!event.target.matches('.show-list')) {
-    arrow.classList.remove('close-arrow');
-    arrow.classList.add('open-arrow');
+  
     var dropdown = document.getElementsByClassName("dropdown__list");
     var i;
     for (i = 0; i < dropdown.length; i++) {
