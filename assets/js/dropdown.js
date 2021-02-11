83ce48ad1cf035947242ed0b7caae81df896211c
Andrieli <andrieliluci@gmail.com>
2021-02-11 09:02:49 -0300

    header + dropdown + css organization

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,15 +1,15 @@
 var showList = document.querySelector('.show-list');
 var list = document.querySelector('.dropdown__list');
-var arrow = document.querySelector('.open-arrow');
+var arrow = document.querySelector('.dropdown__icon');
 
 function show() {
-    list.classList.toggle("active");
+    list.classList.toggle('active');
+    arrow.classList.toggle('dropdown__icon-opacity');
 }
   
 
 window.onclick = function(event) {
     if (!event.target.matches('.show-list')) {
-  
     var dropdown = document.getElementsByClassName("dropdown__list");
     var i;
     for (i = 0; i < dropdown.length; i++) {
